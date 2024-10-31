d77b246219a6f642272214e2fcb374af311dbe1b
TiagoCunha85 <114116621+TiagoCunha85@users.noreply.github.com>
2024-10-31 13:08:19 -0300

    Refactor ConnectApplication and ClinicaOuHospitalModel classes
    - Add RestController annotation to ConnectApplication class
    - Create HomeController class with a home() method returning a welcome message
    - Update ClinicaOuHospitalModel class to use List instead of single objects for contatos and enderecos

--- src/main/java/com/connect/jpa/ConnectApplication.java
+++ src/main/java/com/connect/jpa/ConnectApplication.java
@@ -2,13 +2,22 @@ package com.connect.jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-//comentário de teste
 @SpringBootApplication
 public class ConnectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConnectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectApplication.class, args);
+    }
+}
 
+@RestController
+class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Bem-vindo ao Connect Azul!";
+    }
 }

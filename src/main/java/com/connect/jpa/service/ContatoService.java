3e746ca5edc2d7cd13d5691405724fc6c6e0439a
Arthur Holanda <holandaarthur46@gmail.com>
2024-11-11 09:44:34 -0300

    Criando camada de serviço de Contato

---
+++ src/main/java/com/connect/jpa/service/ContatoService.java
@@ -0,0 +1,47 @@
+package com.connect.jpa.service;
+
+import com.connect.jpa.model.ContatoModel;
+import com.connect.jpa.repository.ContatoRepository;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ContatoService {
+
+    private final ContatoRepository repository;
+
+    public ContatoService(ContatoRepository contatoRepository) {
+        this.repository = contatoRepository;
+    }
+
+    public List<ContatoModel> todosContatos() {
+        return repository.findAll();
+    }
+
+    public Optional<ContatoModel> pegarPeloId(long id) {
+        return repository.findById(id);
+    }
+
+    public ContatoModel criarContato(ContatoModel contato) {
+        return repository.save(contato);
+    }
+
+    public Optional<ContatoModel> atualizarContato(long id, ContatoModel contatoAtualizado) {
+        return repository.findById(id).map(record -> {
+            record.setTelefone(contatoAtualizado.getTelefone());
+            record.setSite(contatoAtualizado.getSite());
+            record.setRedeSocial(contatoAtualizado.getRedeSocial());
+            return repository.save(record);
+        });
+    }
+
+    public boolean deletarContato(long id) {
+        return repository.findById(id).map(record -> {
+            repository.deleteById(id);
+            return true;
+        }).orElse(false);
+    }
+}

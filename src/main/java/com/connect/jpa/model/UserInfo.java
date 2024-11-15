d636f79e7bad1c16fcc1eed516b3a6acf4a2143a
Arthur Holanda <holandaarthur46@gmail.com>
2024-11-15 14:20:53 -0300

    Removendo MappedSuperClass

--- src/main/java/com/connect/jpa/model/UserInfo.java
+++ src/main/java/com/connect/jpa/model/UserInfo.java
@@ -4,12 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@MappedSuperclass
+
 @Entity
 @Data
 @AllArgsConstructor

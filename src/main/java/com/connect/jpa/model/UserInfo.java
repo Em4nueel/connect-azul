3bd5f9f7391e058fde8988749c07d7c2c72be471
Joao Emanuel <joaoemanuel20181@outlook.com>
2024-11-18 18:28:32 -0300

    Corrigindo conflito

--- src/main/java/com/connect/jpa/model/UserInfo.java
+++ src/main/java/com/connect/jpa/model/UserInfo.java
@@ -8,11 +8,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
 public class UserInfo {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;

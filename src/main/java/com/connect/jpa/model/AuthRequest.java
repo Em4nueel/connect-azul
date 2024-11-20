6c2707b29f9c08323462466464891fd06f7fb887
Arthur Holanda <holandaarthur46@gmail.com>
2024-11-20 15:03:18 -0300

    Corrigindo AuthRequest

--- src/main/java/com/connect/jpa/model/AuthRequest.java
+++ src/main/java/com/connect/jpa/model/AuthRequest.java
@@ -1,12 +1,9 @@
 package com.connect.jpa.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class AuthRequest {
 	
     private String username; 
